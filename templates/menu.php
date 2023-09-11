bd37e2f19b35ffbcd91dc2e4e46128574346c317
SamesOfAm <flo.tepelmann@ewetel.net>
2023-09-11 11:55:45 +0200

    Initial commit

---
+++ templates/menu.php
@@ -0,0 +1,6 @@
+<div class="mobile-menu-header">
+    {{insert_module::4}} <!-- Mobile Menu Social Media Icons -->
+    <a data-mobile-menu="close"><img src="files/assets/layout/menu-close.svg"></a>
+</div>
+
+{{insert_module::3}} <!-- Mobile Menu Navigation -->

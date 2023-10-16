006c5e183ec005768291143d809e21fc6cafaba5
Florian <flo.tepelmann@ewetel.net>
2023-10-16 10:59:16 +0200

    Kanada

---
+++ templates/footer.php
@@ -0,0 +1,17 @@
+<div class="footer-left">
+    <div class="footer-logo">
+        {{insert_content::33}}  <!-- Eingefügte Inhalte/Footer/1 -->
+    </div>
+    <div class="footer-info">
+        <div class="footer-address">
+            {{insert_content::34}}  <!-- Eingefügte Inhalte/Footer/2 -->
+        </div>
+        <div class="footer-social">
+            {{insert_content::35}}  <!-- Eingefügte Inhalte/Footer/3 -->
+        </div>
+    </div>
+</div>
+<div class="footer-right">
+    <div class="mobile-only white text-center"><h3>Kontakt aufnehmen</h3></div>
+    {{insert_form::2}} <!-- Kontaktformular -->
+</div>
--- templates/menu.php
+++ templates/menu.php
@@ -1,6 +1,16 @@
 <div class="mobile-menu-header">
     {{insert_module::4}} <!-- Mobile Menu Social Media Icons -->
-    <a data-mobile-menu="close"><img src="files/assets/layout/menu-close.svg"></a>
+    <div class="menu-logo">
+        <img src="files/assets/layout/Logo-Niederlassung-in-Thueringen.svg">
+    </div>
+    <a class="mobile-menu-close" data-mobile-menu="close"><img src="files/assets/layout/menu-close.svg"></a>
 </div>
-
-{{insert_module::3}} <!-- Mobile Menu Navigation -->
+<div class="mobile-menu-navigations">
+    {{insert_module::8}} <!-- Mobile Menu Navigation 1 -->
+    {{insert_module::9}} <!-- Mobile Menu Navigation 2 -->
+    {{insert_module::10}} <!-- Mobile Menu Navigation 3 -->
+    {{insert_module::11}} <!-- Mobile Menu Navigation 4 -->
+</div>
+<div class="mobile-menu-footer">
+    <img src="files/assets/layout/Niederlassung-in-Thueringen-Menu-Fusszeile-Logos.svg">
+</div>
